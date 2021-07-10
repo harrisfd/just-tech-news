d254302dba0fdd817a2d73f83a0aa5fe910c73de
ldylicia <fharrisfoster@gmail.com>
2021-07-09 18:26:02 -0700

    added additonal files

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 
 // get all users
@@ -57,12 +58,12 @@ router.get('/', (req, res) => {
       });
   });
 
-  router.post('/', (req, res) => {
+  router.post('/', withAuth, (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
       title: req.body.title,
       post_url: req.body.post_url,
-      user_id: req.body.user_id
+      user_id: req.session.user_id
     })
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
@@ -70,7 +71,7 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
-  router.put('/:id', (req, res) => {
+  router.put('/:id', withAuth, (req, res) => {
     Post.update(
       {
         title: req.body.title
@@ -94,7 +95,7 @@ router.get('/', (req, res) => {
       });
   });
 
-  router.delete('/:id', (req, res) => {
+  router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
       where: {
         id: req.params.id
@@ -112,3 +113,5 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  module.exports=router
